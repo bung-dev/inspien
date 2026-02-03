7b8bc7d169b63c410e7fd224508b7bb92fe43e24
Bung <wndgh26@gmail.com>
2026-02-03 14:44:02 +0900

    refactor: reorganize package structure

--- src/main/java/com/inspien/common/config/JdbcConfig.java
+++ src/main/java/com/inspien/common/config/JdbcConfig.java
@@ -1,4 +1,4 @@
-package com.inspien.receiver.jdbc;
+package com.inspien.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/inspien/common/config/SftpConfig.java
+++ src/main/java/com/inspien/common/config/SftpConfig.java
@@ -1,4 +1,4 @@
-package com.inspien.receiver.sftp;
+package com.inspien.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
